d92aa3b4a974c18c4c17886c457d74fa59670069
dongfang <1790956510@qq.com>
2022-06-07 16:07:15 +0800

    feat(配置): 配置别名，配置主机

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+/*
+ * @Descripttion: 
+ * @Author: zhaodongfeng
+ * @Date: 2022-06-07 10:15:45
+ * @LastEditors: zhaodongfeng
+ * @LastEditTime: 2022-06-07 16:04:46
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const path = require('path')
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    // 配置别名
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  // 配置主机
+  server:{
+    host: '0.0.0.0',
+  }
 })
